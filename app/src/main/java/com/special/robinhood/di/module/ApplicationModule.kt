26ad8d96d4c3b8dbe1a5c13bf970d82118cc3f3f
thanes.sriamornruttanakul <thanes.sriamornruttanakul@scb.co.th>
2020-04-27 12:36:12 +0700

    connect api not success

--- app/src/main/java/com/special/robinhood/di/module/ApplicationModule.kt
+++ app/src/main/java/com/special/robinhood/di/module/ApplicationModule.kt
@@ -2,21 +2,33 @@ package com.special.robinhood.di.module
 
 import android.app.Application
 import android.content.Context
-import dagger.Module
-import dagger.Provides
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import com.special.robinhood.cache.BufferooCacheImpl
 import com.special.robinhood.cache.PreferencesHelper
 import com.special.robinhood.cache.db.DbOpenHelper
 import com.special.robinhood.cache.mapper.BufferooEntityMapper
-import com.special.robinhood.data.BufferooDataRepository
-import com.special.robinhood.data.executor.JobExecutor
-import com.special.robinhood.data.mapper.BufferooMapper
+import com.special.robinhood.data.LandingRepository
+import com.special.robinhood.data.mapper.LandingMapper
+import com.special.robinhood.data.network.LandingApi
 import com.special.robinhood.data.repository.BufferooCache
-import com.special.robinhood.data.source.BufferooDataStoreFactory
-import com.special.robinhood.domain.executor.PostExecutionThread
 import com.special.robinhood.domain.executor.ThreadExecutor
-import com.special.robinhood.domain.repository.BufferooRepository
-import com.special.robinhood.view.UiThread
+import com.special.robinhood.domain.interactor.ThreadExecutors.OBSERVER_ON
+import com.special.robinhood.domain.interactor.ThreadExecutors.SUBSCRIBER_ON
+import com.special.robinhood.domain.interactor.ThreadExecutors.SUBSCRIBER_ON_COMPUTATION
+import com.special.robinhood.domain.interactor.ThreadExecutors.SUBSCRIBER_ON_IO
+import com.special.robinhood.domain.repository.LandingRepositoryContractor
+import dagger.Module
+import dagger.Provides
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Named
+import javax.inject.Singleton
 
 /**
  * Module used to provide dependencies at an application-level.

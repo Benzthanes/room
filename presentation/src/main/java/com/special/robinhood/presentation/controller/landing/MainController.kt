4baf1e47d800b30ffe3d76d6062d032fe366abf5
Sittipong Saladaeng <>
2020-04-27 14:24:52 +0700

    add BaseController

---
+++ presentation/src/main/java/com/special/robinhood/presentation/controller/landing/MainController.kt
@@ -0,0 +1,23 @@
+package com.special.robinhood.presentation.controller.landing
+
+import com.special.robinhood.domain.interactor.BaseUseCase
+import com.special.robinhood.domain.interactor.landing.MainUseCase
+import com.special.robinhood.domain.model.Mobile
+import com.special.robinhood.presentation.BaseController
+import io.reactivex.disposables.CompositeDisposable
+import javax.inject.Inject
+
+class MainController
+@Inject constructor(
+        private val useCase: MainUseCase
+) : BaseController() {
+
+    fun callGetPhone(onSuccess: (List<Mobile>) -> Unit,
+                     onError: (Throwable) -> Unit) {
+        useCase.execute(
+                onSuccess,
+                onError,
+                BaseUseCase.VoidParams(),
+                CompositeDisposable())
+    }
+}

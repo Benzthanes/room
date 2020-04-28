2a5b59cd8f894a1d3240fb3156e0fae84ed81809
Sittipong Saladaeng <>
2020-04-28 12:02:41 +0700

    add ImageViewExt

---
+++ app/src/main/java/com/special/robinhood/extensions/ViewExt.kt
@@ -0,0 +1,15 @@
+package com.special.robinhood.extensions
+
+import android.view.View
+
+fun View.gone() {
+    this.visibility = View.GONE
+}
+
+fun View.invisible() {
+    this.visibility = View.INVISIBLE
+}
+
+fun View.visible() {
+    this.visibility = View.VISIBLE
+}
